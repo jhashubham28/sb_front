85ff108a65ed5ee0bd357f85999e23d71989400c
Shubham Jha <jhashubham28@gmail.com>
2018-06-21 14:44:06 +0530

    Server Message

--- Server.js
+++ Server.js
@@ -2,9 +2,6 @@
 var express = require('express');
 var app = express(); 						// create our app w/ express
 var port = process.env.PORT || 8080; 				// set the port
-// routes ======================================================================
-require('./app/routes.js')(app);
 
-// listen (start app with node server.js) ======================================
 app.listen(port);
 console.log("App listening on port " + port);
