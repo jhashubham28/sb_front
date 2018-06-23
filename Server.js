ab91283cfdd8f4062ca3701f0c894d650eb3a894
Ubuntu <ubuntu@ip-172-31-20-180.ec2.internal>
2018-06-23 07:28:17 +0000

    23-06-2018

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 var express = require('express'); 
 var app = express();                                            // create our app w/ express   
 var port = process.env.PORT || 8080;                            // set the port 
- app.use(express.static("src"));                                                                                                                                                                                                                 
+ app.use(express.static("dist"));                                                                                                                                                                                                                 
  app.get('/',function(req, res){                                                                                        
   res.redirect('/');                                                                                                      
 });                                            
