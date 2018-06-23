86c72f1ec1d23e1f92c32d14c17ba55e062fea66
Shubham Jha <jhashubham28@gmail.com>
2018-06-23 11:17:57 +0530

    Changed index

--- Server.js
+++
@@ -1,7 +0,0 @@
-
-var express = require('express');
-var app = express(); 						// create our app w/ express
-var port = process.env.PORT || 8080; 				// set the port
-
-app.listen(port);
-console.log("App listening on port " + port);
